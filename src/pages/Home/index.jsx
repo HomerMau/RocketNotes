0bd45bd1faf0f8f5dc26fdf16f6f9ac8faa49a37
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-02-02 23:51:15 -0300

    ButtonCreateNotes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
-
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 
@@ -22,7 +22,10 @@ export function Home(){
 
       <Content></Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
     </Container>
   )
 }
