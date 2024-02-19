db3ee442560e116234466ec05c19135b631f2a38
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-02-19 11:32:01 -0300

    feat: hooks File

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react"
+import { createContext } from "react"
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+  return (
+    <AuthContext.Provider
+      value={{ name: "Rodrigo", email: "rodrigo@email.com" }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth(){
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth }
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from "./styles/global"
 
 import theme from "./styles/theme"
 
-import { MyContext } from "./myContext"
+import { AuthProvider } from "./hooks/auth"
 
 import { Routes } from "./routes"
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MyContext.Provider value={{ name: 'Rodrigo', email: 'rodrigo@email.com'}} >
-      <Routes />
-      </MyContext.Provider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
