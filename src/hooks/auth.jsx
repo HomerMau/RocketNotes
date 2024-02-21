2e5e2864d20b2cfb0f82ac2f97638d338c7672ad
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-02-21 15:13:42 -0300

    feat: LocalStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { useContext, createContext, useState } from "react"
+import { useContext, createContext, useState, useEffect } from "react"
 
 import { api } from "../services/api"
 
@@ -10,7 +10,10 @@ function AuthProvider({ children }) {
   async function signIn({ email, password }) {
     try {
       const response = await api.post("/sessions", { email, password })
-      const { user, token } = response.data
+      const { user, token } = response.data;
+
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
@@ -22,6 +25,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
